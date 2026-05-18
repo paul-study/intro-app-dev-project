f0f52c3ba649af1570b42c1e6826624d643f4783
Paul Johnson (1000138616) <JOHNPM4@student.op.ac.nz>
2026-05-18 14:35:03 +1200

    middleware folder added

--- project-messenger-app/backend/app.js
+++ project-messenger-app/backend/app.js
@@ -4,6 +4,8 @@ import compression from "compression";
 
 import userRoutes from "./routes/user.js"
 
+import isContentTypeApplicationJSON from "./middleware/content-type.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +14,8 @@ const API_BASE_URL = process.env.API_BASE_URL || "http://localhost";
 app.use(cors());
 app.use(compression());
 app.use(express.json()); 
+
+app.use(isContentTypeApplicationJSON); //checking the middleware content type
 
 app.use("/api/users", userRoutes)
 
