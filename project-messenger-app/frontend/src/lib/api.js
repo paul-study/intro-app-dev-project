e9a2b1db0cd27bd83d56b795b49ee5675591f86f
paul-study <paulstudylaptop@gmail.com>
2026-06-26 22:42:53 +1200

    updated the URL for render

--- project-messenger-app/frontend/src/lib/api.js
+++ project-messenger-app/frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
-// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = "http://localhost:3000"
+// const API_URL = "http://localhost:3000"
 
 export async function apiCall(endpoint, options = {}) {
   const url = `${API_URL}${endpoint}`;
